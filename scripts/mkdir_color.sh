cc89ba9377c49c6d97864f58bf75cfa179798ef6
Christian Thomas <christian@skytruth.org>
2024-08-14 16:42:03 -0400

    added scripts dir and mkdir_color.sh

---
+++ scripts/mkdir_color.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+mkdir_r() {
+    cp -R ~/Desktop/General/Folder_Templates/Red ./"$1"
+}
+
+mkdir_p() {
+    cp -R ~/Desktop/General/Folder_Templates/Pink ./"$1"
+}
+
+mkdir_g() {
+    cp -R ~/Desktop/General/Folder_Templates/Green ./"$1"
+}
+
+mkdir_o() {
+    cp -R ~/Desktop/General/Folder_Templates/Orange ./"$1"
+}
+
+mkdir_y() {
+    cp -R ~/Desktop/General/Folder_Templates/Yellow ./"$1"
+}
